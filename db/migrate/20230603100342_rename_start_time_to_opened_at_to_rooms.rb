57207b18b1205246ff5055b59f063e27ad714811
oraul <1838418+oraul@users.noreply.github.com>
2023-06-03 07:22:48 -0300

    Remove bad smell

--- db/migrate/20230603100342_rename_start_time_to_opened_at_to_rooms.rb
+++ db/migrate/20230603100342_rename_start_time_to_opened_at_to_rooms.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RenameStartTimeToOpenedAtToRooms < ActiveRecord::Migration[7.0]
   def change
     rename_column :rooms, :start_time, :opened_at
--- db/migrate/20230603100355_rename_end_time_to_closed_at_to_rooms.rb
+++ db/migrate/20230603100355_rename_end_time_to_closed_at_to_rooms.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RenameEndTimeToClosedAtToRooms < ActiveRecord::Migration[7.0]
   def change
     rename_column :rooms, :end_time, :closed_at
