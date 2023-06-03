ebb0c36bbf5af0fc5ed50eec5649c16b0810ed08
oraul <1838418+oraul@users.noreply.github.com>
2023-06-02 21:42:06 -0300

    Add time committed scope

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -4,4 +4,14 @@ class Schedule < ApplicationRecord
   belongs_to :room
 
   validates :title, :start_time, :end_time, presence: true
+
+  scope :time_committed?, lambda { |start_time, end_time|
+    where("
+      tstzrange(
+        start_time,
+        end_time,
+        '[]'
+      ) && tstzrange(?, ?, '[]')
+    ", start_time, end_time)
+  }
 end
--- spec/models/schedule_spec.rb
+++ spec/models/schedule_spec.rb
@@ -3,11 +3,29 @@
 require 'rails_helper'
 
 RSpec.describe Schedule do
-  subject(:schedule) { build(:schedule) }
+  subject(:schedule) { create(:schedule) }
 
   describe 'validations' do
     it { is_expected.to validate_presence_of(:title) }
     it { is_expected.to validate_presence_of(:start_time) }
     it { is_expected.to validate_presence_of(:end_time) }
+  end
+
+  describe '#time_commited?' do
+    subject(:time_commited?) { described_class.time_committed?(start_time, end_time) }
+
+    context 'when it exists' do
+      let(:start_time) { schedule.start_time }
+      let(:end_time) { schedule.end_time }
+
+      it { is_expected.to exist }
+    end
+
+    context 'when it not exists' do
+      let(:start_time) { schedule.start_time - 1.day }
+      let(:end_time) { schedule.end_time - 1.day }
+
+      it { is_expected.not_to exist }
+    end
   end
 end
